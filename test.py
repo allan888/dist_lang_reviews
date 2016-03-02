f80ea1623ada946e257c77642543eb8a7fff32f3
Jieao Zhu <zhujieao@gmail.com>
2016-03-01 20:03:51 -0500

    bootstrap lib added

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import make_response
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>zhujieaoHel3lo1 2Worl32d1123232323231!</h1>',400
+    response = make_response('web')
+    response.set_cookie('test_cookie_key','test_cookie_value')
+    name = "world"
+    return render_template('index.html', name=name)
 
 @app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, % s!</h1>' % name
+def user(name = 'world'):
+    return render_template('user.html', name=name)
 
 def run_before_first_request():
     pass
@@ -18,4 +27,5 @@ def run_teardown_request(args):
 if __name__ == '__main__':
     app.before_first_request(run_before_first_request)
     app.teardown_request(run_teardown_request)
-    app.run(host= '0.0.0.0',port=80, debug=True)
+    #app.run(host= '0.0.0.0',port=5000, debug=True)
+    manager.run()
