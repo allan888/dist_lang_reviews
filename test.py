4c99cfc5ad6456c0940bab49ce44a7146f74e14f
Jieao Zhu <zhujieao@gmail.com>
2016-03-08 19:02:21 -0500

    bind projects table with Project class in python

--- test.py
+++ test.py
@@ -22,6 +22,27 @@ class Project(db.Model):
     __tablename__ = 'projects'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
+    home_page = db.Column(db.String(500))
+    developer = db.Column(db.String(500))
+    developer_email = db.Column(db.String(500))
+    developer_home_page = db.Column(db.String(200))
+    problem = db.Column(db.String(500))
+    algorithm = db.Column(db.String(200))
+    language = db.Column(db.String(500))
+    language_version = db.Column(db.String(500))
+    release_date = db.Column(db.Date)
+    release_version = db.Column(db.String(500))
+    platforms = db.Column(db.String(500))
+    lines_total = db.Column(db.String(500))
+    lines_pure = db.Column(db.String(500))
+    applications = db.Column(db.String(500))
+    additional_information = db.Column(db.String(500))
+    additional_attributes = db.Column(db.String(500))
+    additional_attributes = db.Column(db.String(500))
+    list_on_dist_algo_web_site = db.Column(db.String(500))
+    submitter = db.Column(db.String(500))
+    submitter_email = db.Column(db.String(500))
+    home_page = db.Column(db.String(500))
     def __repr__(self):
         return '<project % r>' % self.title
 
@@ -30,10 +51,8 @@ def index():
     response = make_response('web')
     response.set_cookie('test_cookie_key','test_cookie_value')
     projects = Project.query.all()
-    print Project
-    print projects
-    name = "world"
-    return render_template('index.html', name=name)
+    #print projects[0].title
+    return render_template('index.html', projects=projects)
 
 @app.route('/user/<name>')
 def user(name = 'world'):
@@ -48,5 +67,5 @@ def run_teardown_request(args):
 if __name__ == '__main__':
     app.before_first_request(run_before_first_request)
     app.teardown_request(run_teardown_request)
-    #app.run(host= '0.0.0.0',port=5000, debug=True)
-    manager.run()
+    app.run(host= '0.0.0.0',port=5000, debug=True)
+    #manager.run()
